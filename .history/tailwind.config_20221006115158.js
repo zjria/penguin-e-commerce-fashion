6c4de07349ba725036f6db547c70a300cb580d4b
zjria <zakaria.com.bd@gmail.com>
2022-10-07 11:11:05 +0600

    first commit

---
+++ .history/tailwind.config_20221006115158.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      fontFamily: {
+        header_text: ['Bebas Neue',
+            'cursive'
+        ],
+        anther_text: ['Roboto',
+            'sans-serif'
+        ]
+    }
+    },
+    plugins: [],
+  }
